b78b7388a4800f564a8addd6ad320e8e47056209
DmytroAF <fedorchenko.d@gmail.com>
2020-03-16 20:15:49 +0200

    ignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hello world");
+        char c = '*';
+        System.out.println(c);
+        char c1 = 51;
+        System.out.println(c1);
     }
+
 }
